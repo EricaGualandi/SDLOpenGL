542c66377c3c6252d6879a43914610df524da195
EricaGualandi <i7679680@bournemouth.ac.uk>
2016-02-22 11:39:38 +0000

    got window working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include "SDLOpenGL.h"
+#include <ngl/NGLInit.h>
+
+void initializeGL();
+void renderGL();
 
 int main(int argc, char *argv[])
 {
+  SDLOpenGL window("Godly test",0,0,1024,720);
   std::cout<<"working!\n";
+  window.makeCurrent();
+  initializeGL();
+  while(true)
+  {
+        renderGL();
+        window.swapWindow();
+        SDL_Delay(10);
+}
+  //while(1){}
+}
+
+void initializeGL()
+{
+  ngl::NGLInit::instance();
+  glClearColor(0.2, 0.8, 0.8, 1.0);
+}
+
+void renderGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 }
